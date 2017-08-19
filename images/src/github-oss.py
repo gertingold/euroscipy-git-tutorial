63b98ed45baddce5895a849e6fc14f30f42dd416
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2017-08-19 21:11:01 +0200

    figure splitted in steps

--- images/src/github-oss.py
+++ images/src/github-oss.py
@@ -1,3 +1,4 @@
+import os, sys
 from pyx import canvas, color, deco, deformer, path, style, text, trafo
 
 def repo(label, colorfg, colorbg):
@@ -40,6 +41,8 @@ def write(size, color):
     cwrite.stroke(p, [color, trafo.scale(size).rotated(-30).translated(0,-0.4*size)])
     return cwrite
 
+basename = os.path.splitext(sys.argv[0])[0]
+
 text.set(text.LatexRunner)
 c = canvas.canvas()
 
@@ -47,6 +50,8 @@ wd = 6.7
 ht = 3.5
 githubfgcolor = color.grey(0.4)
 githubbgcolor = color.grey(0.97)
+maintainercolor = color.hsb(0.7, 1, 0.5)
+usercolor = color.hsb(0.05, 1, 0.5)
 c.stroke(path.rect(0, 0, wd, ht), [deformer.smoothed(0.1),
                                    githubfgcolor,
                                    style.linewidth.Thick,
@@ -60,17 +65,41 @@ clabel.fill(labelbox.path(), [githubfgcolor, deformer.smoothed(0.1)])
 clabel.insert(labeltext)
 c.insert(clabel, [trafo.translate(extrawd, ht+extrawd)])
 
+c.text(wd+0.4, ht-0.5, r'\footnotesize\textsf{read/write permissions}')
+c.insert(read(1, usercolor), [trafo.translate(wd+0.7, ht-0.9)])
+c.insert(write(1, usercolor), [trafo.translate(wd+1.05, ht-0.9)])
+c.text(wd+1.5, ht-1.0, r'\footnotesize\textsf{user}', [usercolor])
+c.insert(write(1, maintainercolor), [trafo.translate(wd+1.05, ht-1.3)])
+c.insert(read(1, maintainercolor), [trafo.translate(wd+0.7, ht-1.3)])
+c.text(wd+1.5, ht-1.4, r'\footnotesize\textsf{maintainer}', [maintainercolor])
+
 c.insert(repo('upstream', color.hsb(0.55, 1, 0.6), color.hsb(0.55, 0.1, 1)),
          [trafo.translate(0.5, 2.8)])
-c.insert(repo('origin', color.hsb(0.13, 1, 0.6), color.hsb(0.13, 0.1, 1)),
-         [trafo.translate(4.6, 0.8)])
+c.insert(read(1, maintainercolor), [trafo.translate(0.8, 2.35)])
+c.insert(write(1, maintainercolor), [trafo.translate(1.15, 2.35)])
+c.insert(read(1, usercolor), [trafo.translate(1.6, 2.35)])
+
 c.insert(repo('local Git repo', color.hsb(0.04, 1, 0.6), color.hsb(0.04, 0.1, 1)),
          [trafo.translate(2.9, -1.6)])
+c.insert(read(1, usercolor), [trafo.translate(5.4, -1.6)])
+c.insert(write(1, usercolor), [trafo.translate(5.75, -1.6)])
+
+c.writePDFfile('{}_1.pdf'.format(basename))
+
+c.insert(repo('origin', color.hsb(0.13, 1, 0.6), color.hsb(0.13, 0.1, 1)),
+         [trafo.translate(4.6, 0.8)])
+
+c.insert(read(1, maintainercolor), [trafo.translate(5.9, 1.05)])
+c.insert(read(1, usercolor), [trafo.translate(5.9, 0.75)])
+c.insert(write(1, usercolor), [trafo.translate(6.25, 0.75)])
 
 opcolor = color.hsb(0.3, 1, 0.4)
 c.stroke(path.line(2.1, 2.5, 4.4, 1.2),
          [deco.earrow, style.linestyle.dashed, opcolor])
 c.text(2.3, 1.7, r'\textsf{fork}', [opcolor])
+
+c.writePDFfile('{}_2.pdf'.format(basename))
+
 c.text(2.3, 1.4, r'\scriptsize\textsf{(only once)}', [opcolor])
 
 p = path.path(path.moveto(5, 1.3),
@@ -94,24 +123,5 @@ p = path.path(path.moveto(4.8, 0.5),
 c.stroke(p, [deco.earrow, opcolor])
 c.text(3.2, -0.5, r'\textsf{pull}', [opcolor])
 
-maintainercolor = color.hsb(0.7, 1, 0.5)
-usercolor = color.hsb(0.05, 1, 0.5)
-c.insert(read(1, usercolor), [trafo.translate(5.4, -1.6)])
-c.insert(write(1, usercolor), [trafo.translate(5.75, -1.6)])
-c.insert(read(1, maintainercolor), [trafo.translate(0.8, 2.35)])
-c.insert(write(1, maintainercolor), [trafo.translate(1.15, 2.35)])
-c.insert(read(1, usercolor), [trafo.translate(1.6, 2.35)])
-c.insert(read(1, maintainercolor), [trafo.translate(5.9, 1.05)])
-c.insert(read(1, usercolor), [trafo.translate(5.9, 0.75)])
-c.insert(write(1, usercolor), [trafo.translate(6.25, 0.75)])
 
-c.text(wd+0.4, ht-0.5, r'\footnotesize\textsf{read/write permissions}')
-c.insert(read(1, usercolor), [trafo.translate(wd+0.7, ht-0.9)])
-c.insert(write(1, usercolor), [trafo.translate(wd+1.05, ht-0.9)])
-c.text(wd+1.5, ht-1.0, r'\footnotesize\textsf{user}', [usercolor])
-c.insert(write(1, maintainercolor), [trafo.translate(wd+1.05, ht-1.3)])
-c.insert(read(1, maintainercolor), [trafo.translate(wd+0.7, ht-1.3)])
-c.text(wd+1.5, ht-1.4, r'\footnotesize\textsf{maintainer}', [maintainercolor])
-
-
-c.writePDFfile()
+c.writePDFfile('{}_3.pdf'.format(basename))
